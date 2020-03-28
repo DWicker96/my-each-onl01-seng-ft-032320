6651d7d5920fc21ed149e036b313a80ec9f681de
Dakota Wicker <dwicker1996@gmail.com>
2020-03-28 20:52:32 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,14 @@
+list = ["'arel'", "'jon'", "'logan'", "'spencer'"]
 
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+end
+
+my_each(list) {|i| "Hello #{i} what's up?"}
 
 
 
